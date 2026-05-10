427a69d52e5010a820a13eb1a64901e820cd3561
yacienn <yacine.djabali@univ-alger.dz>
2026-05-10 19:16:46 +0200

    Create connect_mysql.php

---
+++ connect_mysql.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "school";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+?>
