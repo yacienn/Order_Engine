fd7187913c124a1839922c819dc39a7de6a2fb26
yacienn <yacine.djabali@univ-alger.dz>
2026-06-06 19:25:25 +0100

    Update Queue.ts

--- Queue.ts
+++ Queue.ts
@@ -1,4 +1,4 @@
-class Queue<T>{
+export class Queue<T>{
     private data: T[] = [] ;
     private head:number = 0 ;
     enqueue(value :T){
@@ -8,8 +8,10 @@ class Queue<T>{
      if (this.isEmpty()) return undefined ;
      const value = this.data[this.head];
      this.head++ ;
+     this.cleanup();
      return value ;
     }
+    
     peek(): T | undefined{
         if(this.isEmpty()) return undefined ;
         return this.data[this.head];
@@ -20,4 +22,14 @@ class Queue<T>{
     size(): number{
         return this.data.length - this.head ;
     }
+    private cleanup(): void {
+    if (this.head > 50) {
+        this.data = this.data.slice(this.head);
+        this.head = 0;
+    }
+     }
+     getAll(){
+        return this.data.slice(this.head);
+     }
 }
+
