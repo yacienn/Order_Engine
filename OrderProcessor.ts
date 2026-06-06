eb695b82ea0f3d75814b7dde3e5411725b3375d2
yacienn <yacine.djabali@univ-alger.dz>
2026-06-06 15:00:00 +0100

    Update OrderProcessor.ts

--- OrderProcessor.ts
+++ OrderProcessor.ts
@@ -1,11 +1,27 @@
 import { Order } from "./Order";
 import { Queue } from "./Queue";
-export class OrderProcessising{
+export class OrderProcessing{
    private queue = new Queue<Order>();
    addOrder(order:Order):void{
-     this.queue.enqueue(order)
+     this.queue.enqueue(order);
+     console.log(`The oder whitch contain ${order.id} - ${order.name} - ${order.price} was Added!`);
    }
-   processOreder(){
-
+   processOrder(){
+    const order = this.queue.dequeue();
+    if (!order){
+      console.log("there is no orders");
+      return ;
+    }
+      console.log(
+      `Order ${order.id} - ${order.name} - ${order.price} was processed`
+    );
+   }
+   showOrders():void{
+      const orders = this.queue.getAll();
+      console.log("Pending orders:\n");
+      orders.forEach((o)=>{
+        console.log(`${o.id} - ${o.name} - ${o.price}`);
+      })
+      
    }
 }
