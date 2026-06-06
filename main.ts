d800892d483d84ed9cf26e554268c70f8038dba8
yacienn <yacine.djabali@univ-alger.dz>
2026-06-06 17:01:40 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,20 @@
-import {OrderProcessing} from './OrderProcessor.js'
+import {OrderProcessing} from './OrderProcessor.ts'
 
 const processor = new OrderProcessing();
-
 processor.addOrder({ id: 1, name: "Laptop", price: 1200 });
 processor.addOrder({ id: 2, name: "Phone", price: 800 });
 processor.addOrder({ id: 3, name: "Keyboard", price: 100 });
 
 processor.showOrders();
 
-processor.processOrder();
-processor.processOrder();
-processor.processOrder();
+const intervalId = setInterval(() => {
+   processor.processOrder();
+   console.log("\n");
+   if (processor.noOrder()) {
+      console.log("No more orders");
+      clearInterval(intervalId);
+      return;
+   }
+   processor.showOrders();
+}, 1800);
 
-processor.processOrder(); // empty case
