e27156610663e04d901e97e66e1d99d30571c184
yacienn <yacine.djabali@univ-alger.dz>
2026-06-09 13:21:12 +0100

    Update OrderProcessor.ts

--- OrderProcessor.ts
+++ OrderProcessor.ts
@@ -1,27 +1,36 @@
-import { Order } from "./Order";
-import { Queue } from "./Queue";
+import type { Order } from "./Order.ts";
+import { Queue } from "./Queue.ts";
 export class OrderProcessing{
    private queue = new Queue<Order>();
    addOrder(order:Order):void{
      this.queue.enqueue(order);
      console.log(`The oder whitch contain ${order.id} - ${order.name} - ${order.price} was Added!`);
    }
-   processOrder(){
-    const order = this.queue.dequeue();
-    if (!order){
-      console.log("there is no orders");
-      return ;
-    }
-      console.log(
+   processOrder():Promise<string | null>{
+    return new Promise((resolve , reject)=>{
+      const order = this.queue.dequeue();
+      if (!order){
+            reject("no orders");
+            return ;
+        } 
+     setTimeout(()=>{
+       resolve(
       `Order ${order.id} - ${order.name} - ${order.price} was processed`
     );
+     },2500)
+    })
+    
+    
    }
    showOrders():void{
       const orders = this.queue.getAll();
-      console.log("Pending orders:\n");
+      console.log("------------------\nPending orders:\n");
       orders.forEach((o)=>{
         console.log(`${o.id} - ${o.name} - ${o.price}`);
-      })
-      
+      });
    }
+   noOrder():boolean{ 
+   return this.queue.isEmpty();
+}
+
 }
