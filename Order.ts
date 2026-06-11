27cecb2642e91adcef87ecec32a3fa061ac9c47c
yacienn <yacine.djabali@univ-alger.dz>
2026-06-11 12:02:19 +0100

    Update Order.ts

--- Order.ts
+++ Order.ts
@@ -1,5 +1,15 @@
-export interface Order {
-    id : number ;
-    name : string ;
-    price : number ;
+export const Status ={
+    PENDING: "PENDING",
+    PROCESSING: "PROCESSING",
+    COMPLETED: "COMPLETED",
+    FAILED: "FAILED"
+} as const;
+
+export type Status = typeof Status[keyof typeof Status];
+
+export interface Order{
+    id: number;
+    name: string;
+    price: number;
+    status: Status;
 }
