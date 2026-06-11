13736635d8f99fbd992d026b371526b74a5c8950
yacienn <yacine.djabali@univ-alger.dz>
2026-06-11 12:01:58 +0100

    Update OrderProcessor.ts

--- OrderProcessor.ts
+++ OrderProcessor.ts
@@ -1,36 +1,47 @@
 import type { Order } from "./Order.ts";
 import { Queue } from "./Queue.ts";
-export class OrderProcessing{
-   private queue = new Queue<Order>();
-   addOrder(order:Order):void{
-     this.queue.enqueue(order);
-     console.log(`The oder whitch contain ${order.id} - ${order.name} - ${order.price} was Added!`);
-   }
-   processOrder():Promise<string | null>{
-    return new Promise((resolve , reject)=>{
-      const order = this.queue.dequeue();
-      if (!order){
-            reject("no orders");
-            return ;
-        } 
-     setTimeout(()=>{
-       resolve(
-      `Order ${order.id} - ${order.name} - ${order.price} was processed`
-    );
-     },2500)
-    })
-    
-    
-   }
-   showOrders():void{
-      const orders = this.queue.getAll();
-      console.log("------------------\nPending orders:\n");
-      orders.forEach((o)=>{
-        console.log(`${o.id} - ${o.name} - ${o.price}`);
-      });
-   }
-   noOrder():boolean{ 
-   return this.queue.isEmpty();
-}
+import { Status } from './Order.ts';
 
+export class OrderProcessing{
+  private queue = new Queue<Order>();
+
+  addOrder(order: Order): void{
+    this.queue.enqueue(order);
+    console.log(`The order which contains ${order.id} - ${order.name} - ${order.price} was Added!`);
+  }
+
+  processOrder(): Promise<string | null>{
+    return new Promise((resolve, reject) => {
+      const order = this.queue.dequeue();
+      if (!order) {
+        reject("No orders");
+        return;
+      }
+      order.status = Status.PROCESSING;
+      console.log(`Order ${order.id} status : ${order.status}`);
+
+      setTimeout(() => {
+        order.status = Status.COMPLETED;
+        console.log(`Order ${order.id} status changed to: ${order.status}`); 
+        resolve(
+          `Order ${order.id} - ${order.name} - ${order.price}  - ${order.status} `
+        );
+      }, 3500);
+    });
+  }
+
+  showOrders(): void{
+    const orders = this.queue.getAll();
+    console.log("------------------\nPending orders:\n");
+    if (orders.length === 0) {
+      console.log("No pending orders");
+    }
+    orders.forEach((o) => {
+      console.log(`${o.id} - ${o.name} - ${o.price} - ${o.status}`);
+    });
+  }
+
+  noOrder(): boolean{
+    return this.queue.isEmpty();
+  }
 }
